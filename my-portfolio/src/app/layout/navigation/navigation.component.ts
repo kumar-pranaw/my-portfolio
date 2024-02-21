3e2eee53dcd2dbfe427c5705ba9c9c136d7d8c37
Ronit <ronitjaiprakash2525@gmail.com>
2024-02-21 14:44:12 +0530

    final changes part 1

--- my-portfolio/src/app/layout/navigation/navigation.component.ts
+++ my-portfolio/src/app/layout/navigation/navigation.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
+  isDropdownOpen: boolean = false;
 
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
 }
